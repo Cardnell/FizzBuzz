b19c14e97a48b44d546e9473ea1d4f5ca8e17308
Dan Cardnell <dan@cardnell.com>
2017-10-26 02:51:17 +0100

    Add project files.

---
+++ FizzBuzz/Controllers/FizzBuzzController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace FizzBuzz.Controllers
+{
+    public class FizzBuzzController : ApiController
+    {
+        private const string MULTIPLES_OF_THREE = "Fizz";
+        private const string MULTIPLES_OF_FIVE = "Buzz";
+        
+        public string Get(int input)
+        {
+            string output = string.Empty;
+
+            if (input % 3 == 0)
+                output += MULTIPLES_OF_THREE;
+            if (input % 5 == 0)
+                output += MULTIPLES_OF_FIVE;
+
+            return output.Length == 0 ? input.ToString() : output;
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using FizzBuzz.Controllers;
+using NUnit.Framework;
+
+namespace FizzBuzzTests
+{
+    [TestFixture]
+    public class FizzBuzzControllerTests
+    {
+        [SetUp]
+        public void SetupTests()
+        {
+        }
+
+        [TestCase(2)]
+        [TestCase(17)]
+        [TestCase(26)]
+        public void FizzBuzz_IntegersNotDivisibleByFiveOrThree_ReturnsInput(int input)
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(input);
+
+            Assert.That(output, Is.EqualTo(input.ToString()));
+        }
+
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(21)]
+        public void FizzBuzz_TakesMutiplesOfThree_ReturnsFizz(int input)
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(input);
+
+            Assert.That(output, Is.EqualTo("Fizz"));
+        }
+
+        [TestCase(10)]
+        [TestCase(25)]
+        [TestCase(35)]
+        public void FizzBuzz_TakesMutiplesOfFive_ReturnsBuzz(int input)
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(input);
+
+            Assert.That(output, Is.EqualTo("Buzz"));
+        }
+
+        [TestCase(15)]
+        [TestCase(45)]
+        [TestCase(60)]
+        public void FizzBuzz_TakesMultiplesOfThreeAndFive_ReturnsFizzBuzz(int input)
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(input);
+
+            Assert.That(output, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void FizzBuzz_TakesFive_ReturnsBuzz()
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(5);
+
+            Assert.That(output, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void FizzBuzz_TakesThree_ReturnsFizz()
+        {
+            var fizzBuzz = new FizzBuzzController();
+
+            string output = fizzBuzz.Get(3);
+
+            Assert.That(output, Is.EqualTo("Fizz"));
+        }
+
+        // At this point I've tested the functionality, but not the JSON output 
+        //on an actual api call.  This should certainly be tested in an automated fashion, 
+        //but given the framework, I left a speficic xml request to still return xml
+        //which complicated the test somewhat, and given the parameters of the instructions
+        //I was not able to complete it in the specific time.
+    }
+}
